666275e126b408b7e6d7ba809169c5f00369a9e4
bragavictorr <cmswgrwm7n@privaterelay.appleid.com>
2026-07-15 20:45:37 -0300

    trabalhos

--- aula 6/menu-concessionaria2.py
+++ aula 6/menu-concessionaria2.py
@@ -7,15 +7,16 @@
 # Resto do programa
 
 def exibir_menu_infantil():
-    print("Relampago marquinhos")
-    print("hotwheels")
-    print("patrulha Canina")
-    print("thomas e seus amigos")
+    menu_infantil = ["Relampago marquinhos","hotwheels","patrulha Canina","thomas e seus amigos"]
+
+    for index, item in enumerate(menu_infantil):
+        print(f"{index+1}-{item}")
 
 def exibir_menu_normal():
-    print("Toyota -> À partir de R$ 180.000")
-    print("Mercedez -> À partir de R$300.000")
-    print("Fiat -> À partir de R$ 70.000")
+    menu_adulto = ["Toyota -> À partir de R$ 180.000","Mercedez -> À partir de R$300.000","Fiat -> À partir de R$ 70.000"]
+    for index, item in enumerate(menu_adulto):
+        print(f"{index+1}-{item}")
+   
 
 def checar_idade(idade):
     if idade < 18:
