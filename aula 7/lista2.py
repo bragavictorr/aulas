932afd4126995a4238b1da07e64e9424dc4d2a1b
bragavictorr <cmswgrwm7n@privaterelay.appleid.com>
2026-07-15 21:50:28 -0300

    aula 7

---
+++ aula 7/lista2.py
@@ -0,0 +1,8 @@
+numeros = [5,4,3,2,1]
+print(len(numeros))
+
+numeros.sort()
+print(numeros)
+
+numeros.reverse()
+print(numeros)
---
+++ aula 7/listas.py
@@ -0,0 +1,25 @@
+#comandos na posição especifica
+lista_de_compras = ["maça","uva"]
+
+#Adiciona no final
+lista_de_compras.append("café")
+
+
+# adiciona na posição específica
+lista_de_compras.insert(0,"pão")
+
+
+# junta lista
+segunda_lista = ["manteiga"]
+lista_de_compras.extend(segunda_lista)
+
+
+lista_de_compras.remove("café")
+
+
+lista_de_compras.pop(0)
+
+
+lista_de_compras.clear()
+print(lista_de_compras)
+
