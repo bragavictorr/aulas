4db1e357e8b493d6171a1c354edeb2548710098c
bragavictorr <cmswgrwm7n@privaterelay.appleid.com>
2026-09-18 21:32:50 -0300

    aula 7

---
+++ unidade 3/aula 7/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html' )
+
+def inicio(request):
+    return render(request, 'index.html')
